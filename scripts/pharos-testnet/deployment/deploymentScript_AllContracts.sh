0e7886ec2e1ccaf9454940389229c6d1e50ec1da
masaun <masaun@protonmail.com>
2025-04-29 18:35:33 +0900

    [SC script - deployment]: Modify the 'path' of forge script

--- scripts/pharos-testnet/deployment/deploymentScript_AllContracts.sh
+++ scripts/pharos-testnet/deployment/deploymentScript_AllContracts.sh
@@ -2,7 +2,7 @@ echo "Load the environment variables from the .env file..."
 . ./.env
 
 echo "Deploying the smart contracts (icl. UltraVerifier, JobTitlesAndSkillsProofVerifier) on Pharos Testnet..."
-forge script scripts/swellchain-testnet/deployment/DeploymentAllContracts.s.sol \
+forge script scripts/pharos-testnet/deployment/DeploymentAllContracts.s.sol \
     --broadcast \
     --rpc-url ${PHAROS_TESTNET_RPC} \
     --chain-id ${PHAROS_TESTNET_CHAIN_ID} \
@@ -12,7 +12,7 @@ forge script scripts/swellchain-testnet/deployment/DeploymentAllContracts.s.sol 
     --skip-simulation
 
 echo "Verify the deployed-smart contracts (icl. UltraVerifier, JobTitlesAndSkillsProofVerifier) on Pharos Testnet Explorer..."
-forge script scripts/swellchain-testnet/deployment/DeploymentAllContracts.s.sol \
+forge script scripts/pharos-testnet/deployment/DeploymentAllContracts.s.sol \
     --rpc-url ${PHAROS_TESTNET_RPC} \
     --chain-id ${PHAROS_TESTNET_CHAIN_ID} \
     --private-key ${PHAROS_TESTNET_PRIVATE_KEY} \
